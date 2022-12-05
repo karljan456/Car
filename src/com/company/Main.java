35530c02adbaaca36b1784739daf6f3d8c69d8ef
karljan456 <75903246+karljan456@users.noreply.github.com>
2022-12-05 13:27:24 +0200

    adding a Car class to the project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,3 +6,48 @@ public class Main {
 	// write your code here
     }
 }
+class Car {
+
+    public String brand;
+    public int model;
+    public int amountOfFuel;
+
+    public Car(String brand, int model, int amountOfFuel){
+
+        this.brand = brand;
+        this.model = model;
+        this.amountOfFuel = amountOfFuel;
+
+    }
+
+
+    public void brake() {
+
+        System.out.println("Car is breaking");
+
+    }
+
+    public void accelerate() {
+
+        if (amountOfFuel > 0) {
+
+            System.out.println("Car is accelerating");
+
+            amountOfFuel--;
+        }else{
+
+            System.out.println("Refill the tank!");
+
+        }
+
+
+    }
+
+    public void printData() {
+
+        System.out.println("The brand is " + brand + ". The model is " + model + ". Amount of fuel left: "
+                + amountOfFuel + " litres.");
+
+    }
+
+}
